35d50f73df60d130491b8808bd82c17b45f39c7e
Sunny <sunnywrocks@gmail.com>
2024-12-31 20:45:19 -0800

    tune model

--- SnapFit/OpenAIService.swift
+++ SnapFit/OpenAIService.swift
@@ -19,7 +19,7 @@ class OpenAIService {
         - Round to nearest 0.1%
         - Do not include explanations or commentary
         - Do not hedge or qualify your estimate
-        - Include
+        - Include only
 
         ASSESSMENT CRITERIA:
         - Primary indicators:
